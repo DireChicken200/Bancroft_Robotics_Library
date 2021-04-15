3112c2ccc10bff87d4dda66dbab61cb675bfc405
DireChicken200 <35498545+DireChicken200@users.noreply.github.com>
2021-04-14 20:19:57 -0400

    Initial Commit

---
+++ Legacy/src/main.cpp
@@ -0,0 +1,18 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       -name here-                                               */
+/*    Created:      -date here-                                               */
+/*    Description:  -description here-                                        */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+#include "vex.h"
+
+using namespace vex;
+
+int main() 
+{
+  vexcodeInit(); // Initializing Robot Configuration. DO NOT REMOVE!
+  
+}
